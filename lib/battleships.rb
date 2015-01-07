63cb16518251fe03ad4460e7fe6f29953abf18a2
Jack Rubio <jackrubio26@gmail.com>
2015-01-07 13:59:56 +0000

    new game link

--- lib/battleships.rb
+++ lib/battleships.rb
@@ -1,9 +1,18 @@
 require 'sinatra/base'
 
+
+
 class Battleships2 < Sinatra::Base
+  set :views, Proc.new { File.join(root, "..", "views")}
   get '/' do
-    'Hello Battleships2!'
+    erb :index
   end
+
+  get '/new_game' do
+    "What's your name?"
+  end
+
+
 
   # start the server if ruby file executed directly
   run! if app_file == $0
