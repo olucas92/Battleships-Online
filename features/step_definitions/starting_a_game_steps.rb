53ba8c6ce5c2259be15f20fad0c2dc930c32d1b3
Oliver <o.lucas@live.com>
2015-01-08 17:26:16 +0000

    Merge conflicts fixed?

--- features/step_definitions/starting_a_game_steps.rb
+++ features/step_definitions/starting_a_game_steps.rb
@@ -14,17 +14,16 @@ Given(/^I am on newgame$/) do
   visit '/new_game'
 end
 
-<<<<<<< HEAD
 When(/^I press the submit button within newgame$/) do
   pending # express the regexp above with the code you wish you had
 end
 
 Then(/^I should be able to submit my name$/) do
   pending # express the regexp above with the code you wish you had
-=======
+end
+
 When(/^click the "(.*?)" button$/) do |arg1|
  click_button(arg1)
->>>>>>> 8c8ca505375be0923b8b77a4b5a5bceeee1dfc31
 end
 
 When(/^I enter "(.*?)" into the "(.*?)" box$/) do |arg1, arg2|
